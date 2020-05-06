7709354d5c973b9b5aa968403b046ed9f79f3549
GLR-901 <gabriel.l.romero@gmail.com>
2020-05-05 21:17:08 -0400

    Routes have been setup, testing and model are required.

--- controllers/characterSheet.js
+++ controllers/characterSheet.js
@@ -1,6 +1,7 @@
 // Dependencies for router
 const express= require('express');
 const router = express.Router();
+const CharacterSheet = require('../models/characterSheet.js'); // .. to get up to the main page since this is located in a folder
 
 
 //Routes
@@ -8,36 +9,66 @@ const router = express.Router();
 ////////////////
 //// Index GET /DNDCS
 ////////////////
-
+router.get('/', (req, res) => {
+    CharacterSheet.find({},(error, allCharacter) =>{
+        res.render('Index', {character: allCharacter});
+    });
+});
 
 ////////////////
 //// Show GET /DNDCS/:id
 ////////////////
-
+router.get('/:id', (req, res) => {
+    CharacterSheet.findById(req.params.id, (err, foundCharacter) => {
+        res.render('Show', {character: foundCharacter});
+    });
+});
 
 ////////////////
 //// New GET /DNDCS/new
 ////////////////
-
+router.get('/new', (req, res) => {
+    res.render('New');
+});
 
 ////////////////
 //// Create POST /DNDCS
 ////////////////
-
+router.post('/', (req, res) => {
+    CharacterSheet.create(req.body, (error, createdCharacter) =>{
+        // Once created - respond to client
+        res.redirect('/DNDCS');
+    })    
+});
 
 ////////////////
 //// Edit GET /DNDCS/:id/edit
 ////////////////
-
+router.get('/:id/edit', (req, res) => {
+    // Find our document from the collection - using mongoose model
+    CharacterSheet.findById(req.params.id, (err, foundCharacter) => {
+        // render the edit view and pass it the foundd fruit
+        res.render('Edit', {character: foundCharacter});
+    });
+});
 
 ////////////////
 //// Update
 ////////////////
-
+router.put('/:id', (req, res) => {
+    CharacterSheet.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedModel) => {
+        res.redirect('/DNDCS');
+    });
+});
 
 ////////////////
 //// Destroy
 ////////////////
+router.delete('/:id', (req, res) => {
+    CharacterSheet.findByIdAndRemove(req.params.id, (error, deletedCharacter) => {
+        res.redirect('/DNDCS');
+    })
+});
 
 // Export router
 module.exports = router;
--- server.js
+++ server.js
@@ -2,15 +2,25 @@
 const express = require('express'); // Invoking the requiring of express
 const app = express(); // Declaring the usage of express
 const port = 3000; // Setting a port number to a var
+const mongoose = require('mongoose'); // Setup for mongoose
+const methodOverride = require('method-override'); // Var for method override
 
 // Middleware required for the use of several methods and features
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:false}));
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(methodOverride('_method')); // to inform that _method is to use alternate method mentioned
 
 // Mongoose connection for access to mongoDB
+mongoose.connect('mongodb://localhost:27017/basiccrud', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+    console.log('Connected to mongo');
+});
 
 // Controller Routes for RESTful routes
 const characterSheetController = require('./controllers/characterSheet.js');
 app.use('/DNDCS', characterSheetController); // any instance of /DNDCS will cause it to looking within that folder location for routes
-
 
 // Listener
 app.listen(port, () => {
