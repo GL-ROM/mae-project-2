2183ead17ec10b08efa6739f3017cc60b479d169
GLR-901 <gabriel.l.romero@gmail.com>
2020-05-09 00:46:45 -0400

    added heroju and port updates

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // Dependencies for project
 const express = require('express'); // Invoking the requiring of express
 const app = express(); // Declaring the usage of express
-const port = 3000; // Setting a port number to a var
 const mongoose = require('mongoose'); // Setup for mongoose
 const methodOverride = require('method-override'); // Var for method override
+
+// Allow use of Heroku's port or your own local port, depending on the environment
+const PORT = process.env.PORT || 3000;
+
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/dndcs';
 
 // Middleware required for the use of several methods and features
 app.use(express.static('public'));
@@ -13,7 +18,8 @@ app.engine('jsx', require('express-react-views').createEngine());
 app.use(methodOverride('_method')); // to inform that _method is to use alternate method mentioned
 
 // Mongoose connection for access to mongoDB
-mongoose.connect('mongodb://localhost:27017/basiccrud', { useNewUrlParser: true, useUnifiedTopology: true });
+// Connect to Mongo
+mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
 mongoose.connection.once('open', () => {
     console.log('Connected to mongo');
 });
@@ -23,6 +29,6 @@ const characterSheetController = require('./controllers/characterSheet.js');
 app.use('/DNDCS', characterSheetController); // any instance of /DNDCS will cause it to looking within that folder location for routes
 
 // Listener
-app.listen(port, () => {
-    console.log('Listening on port: ', port);
+app.listen(PORT, () => {
+    console.log('Listening on port: ', PORT);
 });
